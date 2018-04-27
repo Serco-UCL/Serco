eab501429b306811e87a0c0fa3e4222be37b58de
arwillame <arnaud.willame@uclouvain.be>
2018-04-27 10:08:58 +0200

    Man page & translations

---
+++ _templates/en/translation.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the UCL-Serco
+ *
+ * Copyright (C) 2018 Université de Louvain-la-Neuve (UCL-TICE)
+ *
+ * Written by
+ *        Erin Dupuis   (erin.dupuis@uclouvain.be)
+ *        Arnaud Willame (arnaud.willame@uclouvain.be)
+ *        Domenico Palumbo (dominique.palumbo@uclouvain.be)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+$string['emptylist']                        = 'Résultat de la recherche vide quand il n\'y a aucun critère de recherche';
+$string['emptylisttitle']                   = 'Résultat de recherche';
+$string['lang']                   = 'English';
