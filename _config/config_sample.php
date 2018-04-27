c1be60550efa68f325f3994f3b658a3c43b7eb8d
arwillame <arnaud.willame@uclouvain.be>
2018-04-27 10:04:41 +0200

    add config_sample

---
+++ _config/config_sample.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the UCL-Serco
+ *
+ * Copyright (C) 2016 Université de Louvain-la-Neuve (UCL)
+ *
+ * Written by
+ *        Erin Dupuis   (erin.dupuis@uclouvain.be)
+ *        Arnaud Willame (arnaud.willame@uclouvain.be)
+ *        Domenico Palumbo (dominique.palumbo@uclouvain.be)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+return array(  
+    'version' => "20180420",
+    //Database 
+    'DBhost' => "",
+    'DBuser' => "",
+    'DBpass' => "",
+    'DBname' => "",
+    //Language 
+    'defautLocale' => "fr",
+    'MailContact' => "",
+);
